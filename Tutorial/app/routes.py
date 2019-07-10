5c96efb47f8c8868ded0815020448beebd84925d
Siddharth <sidjain1412@gmail.com>
2019-07-10 23:34:39 +0530

    Added basic jinja2 templating

--- Tutorial/app/routes.py
+++ Tutorial/app/routes.py
@@ -2,6 +2,16 @@ from app import app
 from flask import render_template
 
 
+# Multiple decorators for more than 1 URL to give the same return
 @app.route('/')
+@app.route('/index')
 def index():
     return render_template("home.html")
+
+
+# Using jinja2 templates
+@app.route('/hello')
+def hello():
+    # This data gets passed to hello.html
+    user = {'username': 'Sid'}
+    return render_template("hello.html", user=user, title='Home')
--- Tutorial/run.py
+++ Tutorial/run.py
@@ -1,3 +1,5 @@
+# Run this file to start the server. It uses the 'app' folder as a package
+# (through app's __init__.py file)
 from app import app
 
 if(__name__ == "__main__"):
