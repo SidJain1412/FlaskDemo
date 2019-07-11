66608aa4eacfe596b7814d8c5a0054699240a26d
Siddharth <sidjain1412@gmail.com>
2019-07-11 23:30:39 +0530

    Added authorization functions using flask_httpauth

--- Tutorial/app/routes.py
+++ Tutorial/app/routes.py
@@ -2,8 +2,23 @@ from app import app
 from app.forms import LoginForm
 # Flash for messages, make_response for JSON errors (instead of HTML errors)
 from flask import render_template, redirect, flash, make_response, jsonify
+# Using HTTPAuth to increase security by protecting through username and p/w
+from flask_httpauth import HTTPBasicAuth
+
+# Functions can be protected by adding `@auth.login_required` decorator
+auth = HTTPBasicAuth()
 
 
+# Auth password checker
+@auth.get_password
+def get_password(username):
+    # This could refer to a user database, filler for now
+    if(username == "sid"):
+        return 'admin'
+    return None
+
+
+# ERROR HANDLING
 # Modifying 404 to send this custom response
 @app.errorhandler(404)
 def not_found(error):
@@ -17,6 +32,13 @@ def bad_request(error):
     return make_response(jsonify({'error': 'Bad Request. Try again.'}, 400))
 
 
+# Error handling for Unauthorized access (Auth)
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}, 401))
+
+
+# ROUTES
 @app.route('/')
 @app.route('/index')
 def index():
