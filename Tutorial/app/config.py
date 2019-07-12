52a37185680d8f6cf9191c944fc0e2e6893d1572
Siddharth <sidjain1412@gmail.com>
2019-07-12 14:50:08 +0530

    Added database and User, posts models. Adding migrations

--- Tutorial/app/config.py
+++ Tutorial/app/config.py
@@ -1,7 +1,11 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
     # Used to protect against CSRF
     # Check if secret key exists in the environment, else 'admin'
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'admin'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
