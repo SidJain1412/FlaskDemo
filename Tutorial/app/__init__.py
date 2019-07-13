9f325b1b0a68a3f3b54779fb75fe6946b31930f4
Siddharth <sidjain1412@gmail.com>
2019-07-13 18:26:24 +0530

    Temporarily removing email error logging

--- Tutorial/app/__init__.py
+++ Tutorial/app/__init__.py
@@ -17,19 +17,19 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 # Email logging for errors
-if not app.debug:
-    if app.config['MAIL_SERVER']:
-        auth = None
-        if app.config['MAIL_USERNAME'] and app.config['MAIL_PASSWORD']:
-            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
-    secure = None
-    if app.config['MAIL_USE_TLS']:
-        secure = ()
-    mail_handler = SMTPHandler(
-        mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-        fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-        toaddrs=app.config['ADMINS'], subject='Microblog Failure',
-        credentials=auth, secure=secure)
+# if not app.debug:
+#     if app.config['MAIL_SERVER']:
+#         auth = None
+#         if app.config['MAIL_USERNAME'] and app.config['MAIL_PASSWORD']:
+#             auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+#     secure = None
+#     if app.config['MAIL_USE_TLS']:
+#         secure = ()
+#     mail_handler = SMTPHandler(
+#         mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+#         fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+#         toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+#         credentials=auth, secure=secure)
 
 
 from app import routes, models, errors
